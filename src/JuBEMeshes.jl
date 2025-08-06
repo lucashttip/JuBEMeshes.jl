d995b8e4be69bc5c7849bf403ea78995aa33b72f
Lucas Pacheco <lucas.agattipacheco@gmail.com>
2025-08-06 18:27:19 -0300

    Applied formatting

--- src/JuBEMeshes.jl
+++ src/JuBEMeshes.jl
@@ -8,7 +8,6 @@ include("auxfunctions.jl")
 include("bars.jl")
 include("soils.jl")
 
-
 export homog_bar, multi_bar, make_mesh_SSI_layer, make_mesh_SSI_singleLayer
 
 end
--- src/auxfunctions.jl
+++ src/auxfunctions.jl
@@ -35,66 +35,70 @@ function finalize_gmsh(popup)
     return filename_msh
 end
 
-function create_points_curves_surfs(points,lines,curves,surfaces)
+function create_points_curves_surfs(points, lines, curves, surfaces)
     ps = []
     ls = []
     cs = []
     ss = []
-    for i in axes(points,1)
-        push!(ps,gmsh.model.geo.addPoint(points[i,2],points[i,3],points[i,4]))
+    for i in axes(points, 1)
+        push!(ps, gmsh.model.geo.addPoint(points[i, 2], points[i, 3], points[i, 4]))
     end
 
-    for i in axes(lines,1)
-        push!(ls,gmsh.model.geo.addLine(ps[lines[i,2]],ps[lines[i,3]]))
+    for i in axes(lines, 1)
+        push!(ls, gmsh.model.geo.addLine(ps[lines[i, 2]], ps[lines[i, 3]]))
     end
 
-    for i in axes(curves,1)
-        orientation = sign.(curves[i,:])
-        idxs = Int.(abs.(curves[i,:]))
-        push!(cs,gmsh.model.geo.addCurveLoop([orientation[2]*ls[idxs[2]], orientation[3]*ls[idxs[3]], orientation[4]*ls[idxs[4]], orientation[5]*ls[idxs[5]]]))
+    for i in axes(curves, 1)
+        orientation = sign.(curves[i, :])
+        idxs = Int.(abs.(curves[i, :]))
+        push!(
+            cs,
+            gmsh.model.geo.addCurveLoop([
+                orientation[2]*ls[idxs[2]],
+                orientation[3]*ls[idxs[3]],
+                orientation[4]*ls[idxs[4]],
+                orientation[5]*ls[idxs[5]],
+            ]),
+        )
     end
 
-    for i in axes(surfaces,1)
+    for i in axes(surfaces, 1)
         idx = Int(abs(surfaces[i]))
         si = sign(surfaces[i])
-        push!(ss,gmsh.model.geo.addPlaneSurface([si*cs[idx]]))
+        push!(ss, gmsh.model.geo.addPlaneSurface([si*cs[idx]]))
     end
 
-    return ps, ls, cs,ss
-
+    return ps, ls, cs, ss
 end
 
-function create_lines(points,lines)
+function create_lines(points, lines)
     ps = []
     ls = []
-    np = size(points,1)
-    nl = size(lines,1)
+    np = size(points, 1)
+    nl = size(lines, 1)
     for i in 1:np
-        push!(ps,gmsh.model.geo.addPoint(points[i,1],points[i,2],points[i,3]))
+        push!(ps, gmsh.model.geo.addPoint(points[i, 1], points[i, 2], points[i, 3]))
     end
 
     for i in 1:nl
-        push!(ls,gmsh.model.geo.addLine(lines[i,1],lines[i,2]))
+        push!(ls, gmsh.model.geo.addLine(lines[i, 1], lines[i, 2]))
     end
 
     return ps, ls
-
 end
 
-
-function create_points(points,lines)
+function create_points(points, lines)
     ps = []
     ls = []
-    np = size(points,1)
-    nl = size(lines,1)
+    np = size(points, 1)
+    nl = size(lines, 1)
     for i in 1:np
-        push!(ps,gmsh.model.geo.addPoint(points[i,1],points[i,2],points[i,3]))
+        push!(ps, gmsh.model.geo.addPoint(points[i, 1], points[i, 2], points[i, 3]))
     end
 
     for i in 1:nl
-        push!(ls,gmsh.model.geo.addLine(lines[i,1],lines[i,2]))
+        push!(ls, gmsh.model.geo.addLine(lines[i, 1], lines[i, 2]))
     end
 
     return ps, ls
-
 end
